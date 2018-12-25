edc77a9b405c01a14c65e86fcfeed252a4dc45e5
Yevhenii Riabyi <zenia9012@gmail.com>
2018-12-25 12:52:45 +0200

    clean code

--- src/Http/Controllers/ContactController.php
+++ src/Http/Controllers/ContactController.php
@@ -10,10 +10,6 @@ use Yevhenii\Contact\Models\Contact;
 
 class ContactController extends Controller
 {
-	public function index(  ) {
-		return view('contact::contact-form');
-    }
-
 	public function store( Request $request ) {
 		Contact::create($request->except('_token'));
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Http\Request;
 
 Route::group(['namespace' => 'Yevhenii\Contact\Http\Controllers'], function (){
-	Route::get('contact-form', 'ContactController@index')->name('contact');
 	Route::post('contact-form', 'ContactController@store')->name('contact.post');
 });
 
